4820697754f32b4c29320c7cd99bc7ad6e437947
Steve Vu <svu@quinn.com>
2020-03-28 02:54:46 -0700

    Updates

---
+++ precache-manifest.875c93ca41843815894e49bde53cb5e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb59216d7113350ca1bcd58dc228c27",
+    "url": "/react-todo-class-components/index.html"
+  },
+  {
+    "revision": "59b72f9a1defde89e836",
+    "url": "/react-todo-class-components/static/css/main.77468e1e.chunk.css"
+  },
+  {
+    "revision": "b135da312d4421ab7a86",
+    "url": "/react-todo-class-components/static/js/2.ac741738.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-class-components/static/js/2.ac741738.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59b72f9a1defde89e836",
+    "url": "/react-todo-class-components/static/js/main.d2102191.chunk.js"
+  },
+  {
+    "revision": "04811af82e15a220ccc0",
+    "url": "/react-todo-class-components/static/js/runtime-main.1b4e59a6.js"
+  }
+]);
